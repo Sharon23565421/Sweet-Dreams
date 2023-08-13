a8c640fa674a7e1588afbb7db3d1203e747d9674
kosame <Sharon23565421@gmail.com>
2023-08-13 23:31:02 +0800

    0813

--- JS/products.js
+++ JS/products.js
@@ -3,7 +3,7 @@ $(function () {
         let showHeight = 200
         $(".favorable .card").each(function () {
             const cardTop = Math.round($(this).offset().top)
-            if ($(window).scrollTop() >= (cardTop) - $(window).height())  {
+            if ($(window).scrollTop() >= (cardTop) - $(window).height()) {
                 $(this).addClass("animate__animated animate__fadeInUp animate__faster")
             } else {
                 $(this).removeClass("animate__animated animate__fadeInUp animate__faster")
@@ -14,9 +14,9 @@ $(function () {
         $(".trainer .wrap:first").each(function () {
             const cardTop = Math.round($(this).offset().top)
             if ($(window).scrollTop() >= (cardTop + showHeight) - $(window).height()) {
-                $(this).addClass("animate__animated animate__fadeInLeft").css({'right': '0px'})
+                $(this).addClass("animate__animated animate__fadeInLeft").css({ 'right': '0px' })
             } else {
-                $(this).removeClass("animate__animated animate__fadeInLeft").css({'right': '100%'})
+                $(this).removeClass("animate__animated animate__fadeInLeft").css({ 'right': '100%' })
             }
             false
         })
@@ -24,9 +24,9 @@ $(function () {
         $(".trainer .wrap:nth-of-type(3)").each(function () {
             const cardTop = Math.round($(this).offset().top)
             if ($(window).scrollTop() >= (cardTop + showHeight) - $(window).height()) {
-                $(this).addClass("animate__animated animate__fadeInRight").css({'left': '0px'})
+                $(this).addClass("animate__animated animate__fadeInRight").css({ 'left': '0px' })
             } else {
-                $(this).removeClass("animate__animated animate__fadeInRight").css({'left': '100%'})
+                $(this).removeClass("animate__animated animate__fadeInRight").css({ 'left': '100%' })
             }
             false
         })
@@ -34,4 +34,15 @@ $(function () {
 
 
     })
+        
+        $(".pic img, .pic .text").click(function () {
+            $(this).siblings(".about-products").css({ "opacity": "1", "visibility": "visible" }).removeClass("animate__animated animate__zoomOut").addClass("animate__animated animate__zoomIn");
+        });
+    
+        $(".about-products").click(function () {
+            $(this).css({ "opacity": "0", "visibility": "hidden" }).removeClass("animate__animated animate__zoomIn").addClass("animate__animated animate__zoomOut");
+        });
+
+
+
 })
